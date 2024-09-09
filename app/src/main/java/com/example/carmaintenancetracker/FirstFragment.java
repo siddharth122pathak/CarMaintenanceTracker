a335ec6e01903b616ad6f04fa6a9d86e22ccb987
nikoflame <piercenik@gmail.com>
2024-09-09 18:58:58 -0400

    Added barebones Homepage UI

--- app/src/main/java/com/example/carmaintenancetracker/FirstFragment.java
+++ app/src/main/java/com/example/carmaintenancetracker/FirstFragment.java
@@ -27,11 +27,6 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonFirst.setOnClickListener(v ->
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
-        );
     }
 
     @Override
