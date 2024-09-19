bc38f36a4e6260e6b7b0ff40f6c8b7b9ad2e1b25
Teckman240 <twandes@student.fullsail.edu>
2024-09-19 18:28:33 -0400

    Making progress in making the buttons on the bottom of the home page work properly.

--- app/src/main/java/com/example/carmaintenancetracker/VehicleDatabaseHelper.java
+++ app/src/main/java/com/example/carmaintenancetracker/VehicleDatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.carmaintenancetracker;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -12,12 +13,12 @@ public class VehicleDatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "vehicle.db";
     private static final int DATABASE_VERSION = 1;
     private static final String TABLE_NAME = "vehicles";
-    private static final String COLUMN_ID = "id";
-    private static final String COLUMN_MAKE = "make";
-    private static final String COLUMN_MODEL = "model";
-    private static final String COLUMN_YEAR = "year";
-    private static final String COLUMN_LICENSE = "license";
-    private static final String COLUMN_MILES = "miles";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_MAKE = "make";
+    public static final String COLUMN_MODEL = "model";
+    public static final String COLUMN_YEAR = "year";
+    public static final String COLUMN_LICENSE = "license";
+    public static final String COLUMN_MILES = "miles";
 
     //Constructor for creating the SQLiteOpenHelper instance
     public VehicleDatabaseHelper(Context context) {
@@ -27,14 +28,15 @@ public class VehicleDatabaseHelper extends SQLiteOpenHelper {
     //Method when the database is first created
     @Override
     public void onCreate(SQLiteDatabase db) {
-        //Create the table for storing vehicle data
+        // Create the table for storing vehicle data with an "active" column
         String CREATE_VEHICLE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + COLUMN_MAKE + " TEXT, "
                 + COLUMN_MODEL + " TEXT, "
                 + COLUMN_YEAR + " TEXT, "
                 + COLUMN_LICENSE + " TEXT, "
-                + COLUMN_MILES + " TEXT" + ")";
+                + COLUMN_MILES + " TEXT, "
+                + "active INTEGER DEFAULT 0" + ")";
         db.execSQL(CREATE_VEHICLE_TABLE);
     }
 
@@ -69,5 +71,61 @@ public class VehicleDatabaseHelper extends SQLiteOpenHelper {
     public Cursor getAllVehicles() {
         SQLiteDatabase db = this.getWritableDatabase(); //Get a readable database
         return db.rawQuery("SELECT * FROM " + TABLE_NAME, null); //Query to get all vehicles
+    }
+
+    public void setActiveVehicle(int vehicleId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        //Reset all vehicles to inactive
+        ContentValues resetValues = new ContentValues();
+        resetValues.put("active", 0);
+        db.update(TABLE_NAME, resetValues, null, null);  //Reset all vehicles
+
+        //Set the selected vehicle as active
+        ContentValues values = new ContentValues();
+        values.put("active", 1);  // Set active vehicle
+        db.update(TABLE_NAME, values, COLUMN_ID + "=?", new String[]{String.valueOf(vehicleId)});
+
+        db.close();
+    }
+
+    public Cursor getActiveVehicle() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE active = 1 LIMIT 1", null);
+    }
+
+    @SuppressLint("Range")
+    public void swapVehicles(int vehicleId1, int vehicleId2) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        //Get the data for vehicle 1
+        Cursor cursor1 = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE id=?", new String[]{String.valueOf(vehicleId1)});
+        Cursor cursor2 = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE id=?", new String[]{String.valueOf(vehicleId2)});
+
+        if (cursor1.moveToFirst() && cursor2.moveToFirst()) {
+            //Extract data for vehicle 1 (Vehicle 0)
+            ContentValues vehicle1 = new ContentValues();
+            vehicle1.put(COLUMN_MAKE, cursor1.getString(cursor1.getColumnIndex(COLUMN_MAKE)));
+            vehicle1.put(COLUMN_MODEL, cursor1.getString(cursor1.getColumnIndex(COLUMN_MODEL)));
+            vehicle1.put(COLUMN_YEAR, cursor1.getString(cursor1.getColumnIndex(COLUMN_YEAR)));
+            vehicle1.put(COLUMN_LICENSE, cursor1.getString(cursor1.getColumnIndex(COLUMN_LICENSE)));
+            vehicle1.put(COLUMN_MILES, cursor1.getString(cursor1.getColumnIndex(COLUMN_MILES)));
+
+            //Extract data for vehicle 2 (Selected Vehicle)
+            ContentValues vehicle2 = new ContentValues();
+            vehicle2.put(COLUMN_MAKE, cursor2.getString(cursor2.getColumnIndex(COLUMN_MAKE)));
+            vehicle2.put(COLUMN_MODEL, cursor2.getString(cursor2.getColumnIndex(COLUMN_MODEL)));
+            vehicle2.put(COLUMN_YEAR, cursor2.getString(cursor2.getColumnIndex(COLUMN_YEAR)));
+            vehicle2.put(COLUMN_LICENSE, cursor2.getString(cursor2.getColumnIndex(COLUMN_LICENSE)));
+            vehicle2.put(COLUMN_MILES, cursor2.getString(cursor2.getColumnIndex(COLUMN_MILES)));
+
+            //Swap the data between Vehicle 0 and the selected vehicle
+            db.update(TABLE_NAME, vehicle2, COLUMN_ID + "=?", new String[]{String.valueOf(vehicleId1)});  // Update vehicle 0 with vehicle 2 data
+            db.update(TABLE_NAME, vehicle1, COLUMN_ID + "=?", new String[]{String.valueOf(vehicleId2)});  // Update selected vehicle with vehicle 0 data
+        }
+
+        cursor1.close();
+        cursor2.close();
+        db.close();
     }
 }
