d2904451cd19027990e5211e9e7d674628133f3f
Gregorio Chaves <GChaves@student.fullsail.edu>
2024-09-19 20:30:26 -0500

    added class and code to connect to java on my screen new_main_add

---
+++ app/src/main/java/com/example/carmaintenancetracker/addnewmaint.java
@@ -0,0 +1,19 @@
+package com.example.carmaintenancetracker;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class addnewmaint extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_maint_add);
+
+
+    }
+
+
+}
